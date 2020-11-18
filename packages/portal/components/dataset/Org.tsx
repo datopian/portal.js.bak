8ad4e0972abbf722cb69a1f8a70c590f36883165
steveoni <steohenoni2@gmail.com>
2020-11-18 16:27:32 +0100

    [Org.tsx][s]: remove the use of UseQuery and make use of mock data

--- packages/portal/components/dataset/Org.tsx
+++ packages/portal/components/dataset/Org.tsx
@@ -4,34 +4,34 @@ import { ErrorMessage } from '../_shared';
 import { GET_ORG_QUERY } from '../../graphql/queries';
 
 const Org: React.FC<{ variables: any }> = ({ variables }) => {
-  const { loading, error, data } = useQuery(GET_ORG_QUERY, {
-    variables,
-    // Setting this value to true will make the component rerender when
-    // the "networkStatus" changes, so we are able to know if it is fetching
-    // more data
-    notifyOnNetworkStatusChange: true,
-  });
+  // const { loading, error, data } = useQuery(GET_ORG_QUERY, {
+  //   variables,
+  //   // Setting this value to true will make the component rerender when
+  //   // the "networkStatus" changes, so we are able to know if it is fetching
+  //   // more data
+  //   notifyOnNetworkStatusChange: true,
+  // });
 
-  if (error) return <ErrorMessage message="Error loading dataset." />;
-  if (loading) return <div>Loading</div>;
+  // if (error) return <ErrorMessage message="Error loading dataset." />;
+  // if (loading) return <div>Loading</div>;
 
-  const { organization } = data.dataset.result;
+  // const { organization } = data.dataset.result;
   return (
     <>
-      {organization ? (
+      {variables ? (
         <>
           <img
             src={
-              organization.image_url ||
+              variables.image_url ||
               'https://datahub.io/static/img/datahub-cube-edited.svg'
             }
             className="h-5 w-5 mr-2 inline-block"
             alt="org_img"
           />
-          <Link href={`/@${organization.name}`}>
+          <Link href={`/@${variables.name}`}>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a className="font-semibold text-primary underline">
-              {organization.title || organization.name}
+              {variables.title || variables.name}
             </a>
           </Link>
         </>
