5398128b55603a83dfd7fbfed54442383aaf3875
steveoni <steohenoni2@gmail.com>
2020-11-27 15:30:51 +0100

    [feat][s]: auth0 logout api

---
+++ packages/portal/pages/api/logout.tsx
@@ -0,0 +1,10 @@
+import authO from '../../utils/auth0';
+
+export default async function logout(req, res) {
+  try {
+    await authO.handleLogout(req, res, {});
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+}
